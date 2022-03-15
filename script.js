3bf46ff1fce3046f35c15d981a45dc6a6c16c29a
Ganesan R <ganesnr18g202@gmail.com>
2022-03-15 22:49:11 +0530

    made responsive

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 //Netflix clone//
 
-let accordian = document.getElementsByClassName("FAQ-button");
+let accordian = document.getElementsByClassName('FAQ-button')
 for (let i = 0; i < accordian.length; i++) {
-    accordian[i].addEventListener("click", function () {
-        if (this.childNodes[1].classList.contains("fa-plus")) {
-            this.childNodes[1].classList.remove("fa-plus");
-            this.childNodes[1].classList.add("fa-times");
-        } else {
-            this.childNodes[1].classList.remove("fa-times");
-            this.childNodes[1].classList.add("fa-plus");
-        }
-        let content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
-    });
+  accordian[i].addEventListener('click', function () {
+    if (this.childNodes[1].classList.contains('fa-plus')) {
+      this.childNodes[1].classList.remove('fa-plus')
+      this.childNodes[1].classList.add('fa-times')
+    } else {
+      this.childNodes[1].classList.remove('fa-times')
+      this.childNodes[1].classList.add('fa-plus')
+    }
+    let content = this.nextElementSibling
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null
+    } else {
+      content.style.maxHeight = content.scrollHeight + 'px'
+    }
+  })
 }
